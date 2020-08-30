e923ce6064e31cce2d18e6795e868efb725fe11c
Nic Schworer <nic.schworer@gmail.com>
2020-08-30 17:12:53 -0400

    date added, grid created

---
+++ assets/script.js
@@ -0,0 +1,108 @@
+// VARIABLES
+var currentDayEl = $("#currentDay");
+var mainEl = $(".container");
+
+var hours = {
+    "9": '',
+    "10": '',
+    "11": '',
+    "12": '',
+    "13": '',
+    "14": '',
+    "15": '',
+    "16": '',
+    "17": '',
+    "18": '',
+};
+
+var currentHour = moment().hour();
+console.log(currentDayEl);
+console.log(currentHour);
+
+// FUNCTIONS
+
+currentDayEl.html(moment().format('LLLL'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
